361b1e9c8d94a554f075047ee50c1cf189eb17ec
Alex Costa <alex_costa.silva@hotmail.com>
2022-01-30 18:23:14 -0200

    primeiro commit

---
+++ dir_e_help.py
@@ -0,0 +1,15 @@
+"""
+Dir e Help
+
+Utilitários Python para te auxiliar na programação.
+
+dir -> Apresenta todos os atributos e funções/métodos disponíveis para determinado tipo de variável;
+
+dir(tipo de dado/variável)
+
+help -> Apresenta a documentação/como utilizar os atributos/métodos disponíveis
+para determinado tipo de dado ou variável.
+
+hep(tipo de dado/variável.propriedade)
+"""
+print(dir('geek'))
